5fecde3c0794a5ee4af207dcbe2fe39e79fa4043
wqwq886 <35338977+wqwq886@users.noreply.github.com>
2018-11-04 19:17:48 +0800

    111

---
+++ 1.Basic Concepts/4.Number_test.py
@@ -0,0 +1,10 @@
+# 分爲int和float
+# 運算+(加) -(減) *(乘) /(除) %(求余) **(乘方)
+# int于float運算結果為float
+# 使用函數str()括號内數據轉化為String類型
+
+print(0.3*0.1)
+print(0.15*100)
+
+age=23
+message="Happy "+str(age)+"rd Birthday!"
