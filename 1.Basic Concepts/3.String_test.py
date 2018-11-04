f771ac7223517cef6923006b8e748e864a0bbfac
wqwq886 <35338977+wqwq886@users.noreply.github.com>
2018-11-04 16:08:38 +0800

    注釋

--- 1.Basic Concepts/3.String_test.py
+++ 1.Basic Concepts/3.String_test.py
@@ -1,7 +1,8 @@
 # string.title() 字符串首字母大写
 # string.upper() 字符串全部大写
 # string.lower() 字符串全部小写
-
+# string.rstrip() 去除字符串末尾空格
+# 字符串中 \t 插入制表符 \n 插入換行符
 
 
 first_name="ada"
@@ -9,4 +10,4 @@ last_name="wong"
 full_name=first_name+" "+last_name
 
 print (full_name.title())
-print (full_name)
+print (full_name.rstrip())
