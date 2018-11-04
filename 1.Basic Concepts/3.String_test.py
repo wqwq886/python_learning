92a59f7fb9ab5ab09672bea0ff72744552f13854
wqwq886 <35338977+wqwq886@users.noreply.github.com>
2018-11-04 18:45:01 +0800

    增加注釋

--- 1.Basic Concepts/3.String_test.py
+++ 1.Basic Concepts/3.String_test.py
@@ -2,12 +2,17 @@
 # string.upper() 字符串全部大写
 # string.lower() 字符串全部小写
 # string.rstrip() 去除字符串末尾空格
+# string.lstrip() 去除字符串首端空格
+# string.strip() 去除字符串兩端空格
+
 # 字符串中 \t 插入制表符 \n 插入換行符
 
 
-first_name="ada"
+first_name=" ada"
 last_name="wong "
 full_name=first_name+" "+last_name
 
 print (full_name.title())
 print (full_name.rstrip())
+print (full_name.lstrip())
+print (full_name.strip())
