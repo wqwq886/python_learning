5e0ca3484a5cf188e7970e2b9d0b2745117971d4
wqwq886 <35338977+wqwq886@users.noreply.github.com>
2018-11-19 22:55:48 +0800

    完成

--- 1.Basic Concepts/7.list_exercise.py
+++ 1.Basic Concepts/7.list_exercise.py
@@ -4,4 +4,21 @@ print(my_destination) #原有順序
 print(sorted(my_destination)) #字母順序且不保存
 print(my_destination) #測試原順序
 print(sorted(my_destination,reverse=True)) #反字母順序且不保存
+print(my_destination) #測試原順序
 
+print("分割\n一下")
+
+my_destination.reverse()
+print(my_destination)
+
+my_destination.reverse()
+print(my_destination)
+
+my_destination.sort()
+print(my_destination)
+
+my_destination.sort()
+print(my_destination)
+
+finished_destination=my_destination.pop(2)
+print(finished_destination)
