4a7e1c8e15df8ea19c32232760045bd3d87ffcb3
wqwq886 <35338977+wqwq886@users.noreply.github.com>
2018-11-19 22:37:06 +0800

    練習

---
+++ 1.Basic Concepts/7.list_exercise.py
@@ -0,0 +1,7 @@
+my_destination=['lijiang','gugong','gulangyu','Maldives','Mount Fuji']
+
+print(my_destination) #原有順序
+print(sorted(my_destination)) #字母順序且不保存
+print(my_destination) #測試原順序
+print(sorted(my_destination,reverse=True)) #反字母順序且不保存
+
