21b9c3753920e1ca2ce2381be718e11cac481011
wqwq886 <35338977+wqwq886@users.noreply.github.com>
2018-11-22 23:16:05 +0800

    rang test

---
+++ 1.Basic Concepts/9.range_test.py
@@ -0,0 +1,21 @@
+for value in range(1,10,2):
+    print(str(value))
+
+numbers=list(range(2,10,2))
+print(numbers)
+
+chengfang=[]
+for value in range(1,10):
+    chengfang.append(value**2)
+print(chengfang)
+
+for value in range(1,21):
+    print(value)
+
+sums=range(1,1000001)
+print(min(sums))
+print(max(sums))
+print(sum(sums))
+
+for value in range(3,31,3):
+    print(value)
