2f0062cda2d97c32cd829d398eba6d0746203daa
wqwq886 <35338977+wqwq886@users.noreply.github.com>
2018-11-22 22:19:39 +0800

    for loop

---
+++ 1.Basic Concepts/8.loop_for_test.py
@@ -0,0 +1,20 @@
+#注意缩进！，注意冒号！
+
+magicians=['liuqian','dawei','wei']
+for magician in magicians:
+    print(magician.title()+',welcome!')
+print('They are great magicians!\n\n')
+
+list_magicans=[]
+for magician in magicians:
+    list_magicans.append(magician.title())
+print(str(magicians)+'\n\n')
+
+ice_creams=['qiaolezi','binggongchang','shuangcuitong']
+for ice_cream in ice_creams:
+    print('I like '+ice_cream.title()+'!')
+print('\n\n')
+
+for value in range(1,11,2):
+    print(value)
+
