8f4e11d5ba26ed148c6865631b1dc6926de52648
wqwq886 <35338977+wqwq886@users.noreply.github.com>
2018-11-26 21:10:10 +0800

    111

--- 1.Basic Concepts/10.Cutlist_test.py
+++ 1.Basic Concepts/10.Cutlist_test.py
@@ -4,3 +4,10 @@ print(play[0:3])
 print(play[-2])
 
 print(play[-2:])
+
+first_play=play[0:3]
+
+print(first_play)
+
+for name in first_play:
+    print(name.title())
---
+++ 1.Basic Concepts/Tuple_test.py
@@ -0,0 +1,9 @@
+# 元組tuple，值不可變的列表。列表用[] 元組用（）
+
+yuanyins=('a','e','i','o','u')
+
+print(yuanyins)
+
+for yuanyin in yuanyins:
+    print(yuanyin.title())
+
